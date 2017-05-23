6e2829be4cc85e868998ba1928bdb93bdcd4442f
AnaJimenez1990 <ana.jimenez@ujcv.edu.hn>
2017-05-23 16:17:02 -0600

    cambio de clases helper y se agregaron otros

---
+++ app/src/main/java/DatabaseHelper/java.java
@@ -0,0 +1,8 @@
+package DatabaseHelper;
+
+/**
+ * Created by Informatica 2 on 23/5/2017.
+ */
+
+public class java {
+}
--- app/src/main/java/com/example/informatica2/aguascomayagua/MainActivity.java
+++ app/src/main/java/com/example/informatica2/aguascomayagua/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.informatica2.aguascomayagua;
 
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import OpenHelper.SQLite_OpenHelper;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       SQLite_OpenHelper helper = new SQLite_OpenHelper(this);
-        // nos devuelve la base de datos en modo de escritura y lectura
-        SQLiteDatabase db = helper.getWritableDatabase();
-
-
 
 
 
